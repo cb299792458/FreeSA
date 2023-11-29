742ee60d0a80547a8ab6aaf263e4b124d2bfe554
cjsmi <ClarenceSmith90@gmail.com>
2023-11-28 23:38:56 -0500

    install sass for frontend, logo, color+styling changes

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
+import Link from 'next/link';
+import './globals.scss'
+import './header.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="header">
+          <Link id="logo-container" href="/">
+            <img id="logo" src='https://trnkt-dev.s3.amazonaws.com/Logo_Small.png'/>
+            FreeSA
+          </Link>
+          <h1>Header</h1>
+          <span>Hamburger Icon</span>
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
