304a715088da8c02d0d97967c9aa766e2271f1c3
brian <brianrlam@gmail.com>
2023-12-05 19:41:56 -0500

    add email confirm backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const User = require('../models/User');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+
+function generateRandomToken(length = 32) {
+    return crypto.randomBytes(length).toString('hex');
+}
 
 const router = express.Router();
 
@@ -9,7 +14,7 @@ router.get('/', (_req, res) => {
 })
 
 // secret stuff...remove this for production
-router.get('/all', async (_req, res) => {
+router.get('/index', async (_req, res) => {
     try {
         const users = await User.find();
         res.json(users);
@@ -23,8 +28,9 @@ router.post('/signup', async (req, res) => {
     try {
         const {email, password} = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
+        const emailToken = generateRandomToken();
 
-        const newUser = new User({email, hashedPassword});
+        const newUser = new User({email, hashedPassword, emailToken});
         await newUser.save();
 
         res.status(201).json({message: 'Sign Up Successful'});
@@ -38,20 +44,38 @@ router.post('/signin', async (req, res) => {
     try {
         const { username, password } = req.body;
 
-        const user = await User.findOne({ username });
+        const user = await User.findOne({username});
         if (!user) {
-            return res.status(401).json({ error: 'Invalid username or password' });
+            return res.status(401).json({error: 'Invalid username or password'});
         }
 
         const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
         if (!isPasswordValid) {
-            return res.status(401).json({ error: 'Invalid username or password' });
+            return res.status(401).json({error: 'Invalid username or password'});
         }
 
         res.json({ message: 'Sign In Successful' });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error in User Log In' });
+        res.status(500).json({error: 'Internal Server Error in User Log In'});
+    }
+});
+
+router.get('/confirm', async (req, res) => {
+    console.log(req.query)
+    try {
+        const {email, token} = req.query;
+        const user = await User.findOne({email});
+        if (user.emailToken===token) {
+            user.emailConfirmed = true;
+            await user.save();
+            res.json({message: 'Email Confirmed'});
+        } else {
+            res.json({message: 'Invalid Token'});
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Internal Server Error in Email Confirmation'});
     }
 });
 
