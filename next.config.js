a98bb4266833272058e6797e6896abea1a2772c8
brian <brianrlam@gmail.com>
2024-01-01 18:32:55 -0500

    create video api routes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,2 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',  // Specify the API routes you want to proxy
-        destination: 'http://localhost:3001/api/:path*',  // Specify the URL of your Express backend
-      },
-    ];
-  },
-};
+const nextConfig = {}
