2e99de5fa11f11a1f8c4c6b41a2a0e27765fd4d0
brian <brianrlam@gmail.com>
2024-01-01 11:46:50 -0500

    clean folder

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google'
 import Link from 'next/link';
 import SideBar from '@/components/sidebar/SideBar';
 import Hamburger from '@/components/sidebar/Hamburger';
-import logo from 'public/Logo_Small.png';
+import logo from '../../public/Logo_Small.png';
 import './globals.scss'
 import './header.scss'
 import Image from 'next/image';
