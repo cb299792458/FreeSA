c5a19d6f5e78444b33b161ba41fa5054c922b29a
brian <brianrlam@gmail.com>
2024-01-15 19:14:36 -0500

    revert db change

--- src/app/api/videos/route.js
+++ src/app/api/videos/route.js
@@ -5,8 +5,10 @@ export async function POST(req) {
         const data = await req.json();
         const video = {...data, num: parseInt(data.num)};
         
-        const {client} = await connectToDatabase();
-        const db = client.db('freesa-db');
+        // const {client} = await connectToDatabase();
+        // const db = client.db('freesa-db');
+        const db = await connectToDatabase();
+        
         const collection = db.collection('videos');
 
         const result = await collection.insertOne(video);
