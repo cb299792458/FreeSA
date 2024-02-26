37ed8aab07c8f545f9af6911f29e49ab06baab0d
brian <brianrlam@gmail.com>
2024-02-26 14:28:02 -0500

    make basic comment CRUD routes

--- src/app/api/comments/create/route.js
+++ src/app/api/comments/create/route.js
@@ -3,16 +3,16 @@ import { connectToDatabase } from '../../../../../utils/mongodb';
 export async function POST(req) {
     try {
         const data = await req.json();
-        const video = {...data, num: parseInt(data.num)};
+        const comment = {...data, videoNum: parseInt(data.videoNum), createdAt: (new Date())};
         
         const {client} = await connectToDatabase();
         const db = client.db('freesa-db');
         
-        const collection = db.collection('videos');
+        const collection = db.collection('comments');
 
-        const result = await collection.insertOne(video);
+        const result = await collection.insertOne(comment);
 
-        return Response.json({result, video, ok: true});
+        return Response.json({result, comment, ok: true});
     } catch (error) {
         console.error(error);
         return Response.json({error});
