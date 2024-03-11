e588ee493c3146bb6daa09db776480eab4b3f04c
brian <brianrlam@gmail.com>
2024-03-11 14:26:51 -0400

    fetch and show comments

--- src/components/video/comments/Comments.jsx
+++ src/components/video/comments/Comments.jsx
@@ -1,17 +1,34 @@
+'use client'
+import { useEffect, useState } from "react";
 import "./Comments.scss"
-
+import axios from "axios";
 
 export default function Comments({ video }){
+    const {title, difficulty, ytUrl, num} = video;
+    const [comments, setComments] = useState([]);
 
-    const {title, difficulty, ytUrl, comments } = video;
+    useEffect(() => {
+        const getProgress = async () => {
+            const res = await axios.get(`/api/comments/read/${num}`);
+            setComments(res.data.comments || []);
+        }
+        getProgress();
+    }, []);
+
 
     return(
     <div id="comments-container">
         <ul>
-            <li>These will be comments.</li>
-            <li>These will be comments.</li>
-            <li>These will be comments.</li>
-            <li>These will be comments.</li>
+            {comments.map((comment) => {
+                return(
+                    <li key={comment._id}>
+                        <h3>{comment.displayName}</h3>
+                        <h5>{comment.createdAt}</h5>
+                        <p>{comment.text}</p>
+                        {comment.updatedAt && <h5>(Edited at comment.updatedAt)</h5>}
+                    </li>
+                )
+            })}
         </ul>
     </div>
     )
